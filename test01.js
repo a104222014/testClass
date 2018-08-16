8f4b813982ca41a1b6a81b13e0573be0aa7eb95b
a104222014 <42425342+a104222014@users.noreply.github.com>
2018-08-16 15:26:09 +0800

    Update test01.js

--- test01.js
+++ test01.js
@@ -9,7 +9,7 @@ const regtest = bitcoin.networks.testnet
 const bip65 = require('bip65')
 
 //輸入私鑰，教學用，請勿隨意公開私鑰
-var privateKey = "cNcvQphXWjAJ365Y8Tuhti5a6fCh9ftpR3GScZs5vdidBwQqpxT4";
+var privateKey = "cNcvQphXWjAJ365Y8Tuhti5a6fCh9ftpR3GScZs5vdidBwQqpxT4";  //改成自己交易時用的地址產生的私鑰
 
 //產生公鑰跟私鑰
 const keyPair = bitcoin.ECPair.fromWIF(privateKey,regtest);
@@ -21,7 +21,7 @@ const txb = new bitcoin.TransactionBuilder(regtest);
 
 
 //用listunspent 取出最後一筆資料的txid
-txb.addInput('c3acac43c6f0e83538fb5c82ed01379349f5dccf43a8af7bb5d8f0c60fb1fcfc', 0);
+txb.addInput('d8c711964efb92730437e70764d32d8bddc8e9d852c0bf9304f12d0202eab3f4', 1);  //改成自己交易時餘額的txID
 
 //用getnewaddress取得新的位址
 txb.addOutput('2MxfNS8sB1GcCo3GBLmX2PUw277H9zvrn2L', 12)
